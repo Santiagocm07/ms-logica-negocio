a9f437ef63d2de827f414e8de1c3c12a87a44298
Santiagocm07 <santicm50@gmail.com>
2025-03-07 15:10:13 -0500

    Creación del datasource con MySQL, sus modelos y repositorios

---
+++ src/models/venta.model.ts
@@ -0,0 +1,33 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class Venta extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id?: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  numero: number;
+
+  @property({
+    type: 'boolean',
+  })
+  notificada?: boolean;
+
+
+  constructor(data?: Partial<Venta>) {
+    super(data);
+  }
+}
+
+export interface VentaRelations {
+  // describe navigational properties here
+}
+
+export type VentaWithRelations = Venta & VentaRelations;
