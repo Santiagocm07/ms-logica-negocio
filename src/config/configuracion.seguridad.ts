79301a4a0fb0ff00e2782329eb50b6e5b55bf5e7
Santiagocm07 <santicm50@gmail.com>
2025-03-11 07:45:40 -0500

    Se configura la estrategia de autorización de la lógica de negocio con base en la de seguridad. Tambien se le hacen pruebas desde Postman para configurar su funcionamiento

---
+++ src/config/configuracion.seguridad.ts
@@ -0,0 +1,8 @@
+export namespace ConfiguracionSeguridad {
+  export const menuProductoId = "67c88d62a19e421300923c34";
+  export const listarAccion = "listar";
+  export const guardarAccion = "guardar";
+  export const editarAccion = "editar";
+  export const eliminarAccion = "eliminar";
+  export const descargarAccion = "descargar";
+}
